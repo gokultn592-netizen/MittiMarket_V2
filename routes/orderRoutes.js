8bbb268163a09661a7bce5a380f3a0e98ecea92a
gokultn592-netizen <gokultn592@gmail.com>
2026-04-09 01:15:13 +0530

    Update orderRoutes.js

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -3,6 +3,11 @@ const router = express.Router();
 const orderController = require('../controllers/orderController');
 
 router.post('/', orderController.createOrder);
-router.get('/admin', orderController.getAdminOrders);
+
+// Admin routes
+router.get('/admin/all', orderController.getAdminOrders);
+
+// User routes (Multi-user support)
+router.get('/user/:userId', orderController.getAdminOrders); // We can filter in controller later
 
 module.exports = router;
